4c5b6be3344955c961affd274992aeb127c0b9a6
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-05-22 09:26:00 -0500

    a

---
+++ translation-system/src/js/translator.js
@@ -0,0 +1,37 @@
+import i18next from 'i18next';
+import Backend from 'i18next-http-backend'
+
+let language
+
+if(localStorage.getItem('language')){
+    language = localStorage.getItem('language')
+}
+else{
+    language= 'en'
+}
+
+i18next.use(Backend).init({
+    lng: 'en',
+    debug: true,
+    backend: {
+        loadPath: '/locales/{{lng}}/{{ns}}.json'
+    },
+    ns: ['translation'],
+    defaultNS: 'translation'
+}).then(() => updateContent())
+
+function updateContent() {
+    let elementsHTML = document.querySelectorAll('[data-i18n]')
+
+    elementsHTML.forEach(element => {
+        let key = element.getAttribute('data-i18n')
+        element.innerHTML = i18next.t(key)
+    })
+}
+
+window.changeLanguage = function (lng) {
+
+    i18next.changeLanguage(lng).then(()=> updateContent())
+    // i18next.changeLanguage(lng, updateContent())
+    localStorage.setItem('language',lng)
+}
